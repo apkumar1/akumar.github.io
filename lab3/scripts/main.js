6278d5e7182317003d45f17b84cd7e18e80d0538
apkumar298 <48388500+apkumar298@users.noreply.github.com>
2020-06-03 21:48:12 -0400

    Update main.js

--- lab3/scripts/main.js
+++ lab3/scripts/main.js
@@ -62,13 +62,6 @@ function populateListProductChoices(slct1, slct2) {
 		// create a breakline node and add in HTML DOM
 		s2.appendChild(document.createElement("br"));  
 		
-		var img = document.getElementById("../lab3/images/"+String(productName)+".JPG").src;
-		var img2 = document.createElement('img'); 
-            	s2.appendChild(img); 
-		
-		// create a breakline node and add in HTML DOM
-		s2.appendChild(document.createElement("br"));  
-		
 		//price added
 		var price = priceArray[i];
 		var price2 = price.toString();
